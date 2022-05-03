780c7513c5b27079bebc3d8da07a576ed97878b7
Bak David <bak.david96@gmail.com>
2022-05-03 17:33:31 +0300

    Start of using dynamic links in url

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,5 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Products() {
-  return <h1>Products</h1>;
+  return (
+    <section>
+      <ul>
+        <li>Book</li>
+        <li>Carpet</li>
+        <li>Online Course</li>
+      </ul>
+    </section>
+  );
 }
